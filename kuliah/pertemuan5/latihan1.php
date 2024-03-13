cb5e9f98f2fb09c728c67c76eab80d20bbe2fe41
Phikmat9 <phikmat9@gmail.com>
2024-03-13 13:04:52 +0700

    upload tgs 4

--- kuliah/pertemuan5/latihan1.php
+++ kuliah/pertemuan5/latihan1.php
@@ -1,1 +1,21 @@
 <?php
+// 1. membuat array
+$hari =  ['senin' , 'selasa' , 'rabu'] ;
+$bulan = ['januari' , 'februari' , 'maret'];
+$mhs = ['hikmat' , '3.2' , 'pandu'];
+// 2. mencetak array (1)
+// cetak 1 isi array
+echo $hari [0];
+echo "<br>";
+echo $bulan [2];
+echo "<br>";
+echo $mhs [2];
+echo "<hr>";
+
+// 3. manipulasi array
+
+// menghapus isi array
+// di belakang array: array_pop()
+// di depan array: array_shift()
+
+// 4. mencetak array (2) 
---
+++ tugas/tugas4/4b.php
@@ -0,0 +1,30 @@
+<?php
+$perangkat = ['Motherboard', 'Processor', 'Hard Diks', 'PC Coller', 'VGA Card', 'SSD'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas4b</title>
+</head>
+<body>
+    <h2>Macam-macam perangkat keras komputer</h2>
+    <ol>
+        <?php foreach ($perangkat as $item) {?>
+            <li> <?= $item?> </li>
+        <?php } ?>
+    </ol>
+    
+    <h2>Macam-macam perangkat keras komputer baru</h2>
+    <ol>
+        <?php 
+        array_push ($perangkat, 'Card Reader', 'Modem');
+        sort($perangkat);
+        foreach ($perangkat as $item) { ?>
+         <li><?= $item ?></li>
+        <?php } ?>
+    </ol>
+</body>
+</html>
